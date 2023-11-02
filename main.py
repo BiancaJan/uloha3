36bfe34a66b801221b7ee83a4297dcbb52941623
Bianca Janotova <byancajanotova@gmail.com>
2023-11-02 22:17:28 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 13 ka úloha ;)
+cislo = int(input("Zadaj mi číslo:"))
+while cislo !=1:
+  if cislo % 2 == 0:
+    cislo = cislo / 2
+    print(int(cislo))
+  else:
+    cislo = 3 * cislo + 1
+    print(int(cislo))
